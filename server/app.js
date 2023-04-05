8ac6675748424382b4e33a89f0cb0a215f70afc0
jhardin7612 <118298960+jhardin7612@users.noreply.github.com>
2023-04-05 17:27:00 -0400

    all Read and Delete api done

--- server/app.js
+++ server/app.js
@@ -8,8 +8,82 @@ app.use(cors());
 app.use(express.json());
 
 
-
 //Root endpoint
-app.get('/', (req, res) => {res.send('Root End point')});                  
+app.get('/', (req, res) => {res.send('Root End point')}); 
+
+//get all items in db
+app.get('/inventory', (req, res)=> {
+    knex
+    .select('*')
+    .from('item')
+    .then(data => res.status(200).json(data))
+    .catch(err =>
+        res.status(404).json(err)
+    )
+})
+
+//get a specific item
+app.get('/inventory/:item_id', (req, res)=> {
+    knex('item')
+    .where('id', req.params.item_id)
+    .then(data => res.status(200).json(data))
+    .catch(err =>
+        res.status(404).json(err)
+    )
+})
+
+// //get all items in specific manager inventory
+app.get('/inventory/manager/:manager_id', (req, res) => {
+    knex('item')
+    .select('*')
+    .where("manager_id",req.params.manager_id)
+    .then(data => res.status(200).json(data))
+    .catch(err=> res.status(404).json(err))
+})
+
+// //get all registered managers & info
+app.get('/managers', (req, res)=>{
+    knex
+    .select('*')
+    .from('manager')
+    .then(data => res.status(200).json(data))
+    .catch(err =>
+        res.status(404).json(err)
+    )
+})
+
+// //get a specific manager info
+app.get('/managers/:manager_id', (req, res)=>{
+    knex('manager')
+    .select('*')
+    .where("id", req.params.manager_id)
+    .then(data => res.status(200).json(data))
+    .catch(err =>
+        res.status(404).json(err)
+    )
+})
+
+//Delete item
+app.delete('/inventory/:item_id', (req, res) => {
+    knex('item')
+    .where("id",req.params.item_id)
+    .del()
+    .then(data => res.status(200).send("item was deleted"))
+    .catch(err=> res.status(404).json(err))
+})
+
+//Create Item
+// app.post('/inventory', (req, res) => {
+//     knex('item')
+//     .insert(req.body)
+//     .then(data => res.status(201).send("item has been added to your inventory")
+//     .catch(err=> res.status(404).json(err) 
+//     ))
+// })
+
+
+
+
+
 
 app.listen(port, () => console.log(`Express Server is running on port ${port}`));
