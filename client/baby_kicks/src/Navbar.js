b9b67b5f9f999bc62aea8f640f1d3ab521ff2241
jhardin7612 <118298960+jhardin7612@users.noreply.github.com>
2023-04-07 09:37:45 -0400

    about to start styling

--- client/baby_kicks/src/Navbar.js
+++ client/baby_kicks/src/Navbar.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import { useNavigate } from 'react-router';
+import { InventoryContext } from './App';
 
 export const Navbar = () => {
     const navigate = useNavigate();
+    const{ manager, username} = React.useContext(InventoryContext);
+
 
     return(
-        <div>
+        <div className='navbar-container'>
+            {(!manager) ? 
+            <div className='navbar-div'>
             <button onClick={()=> navigate('/Login')}>Login</button>
             <button onClick={()=> navigate('/Register')}>Register</button>
+            </div>
+            :
+            <div className='welcom-div'>
+                <p className='welcome-p'>Welcome, {manager.first_name}</p>
+            </div>
+            }
         </div>
     )
 }
